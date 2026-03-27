58e955927451cef5ef700c019f884cfedf50b2d1
Erein Aimer <ereinaimer@gmail.com>
2026-03-27 16:31:18 +0530

    chore(frb): setup flutter_rust_bridge and scaffold flutter project

---
+++ app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:taurine_app/src/rust/api/simple.dart';
+import 'package:taurine_app/src/rust/frb_generated.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            greet(name: "from Taurine Core"),
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
